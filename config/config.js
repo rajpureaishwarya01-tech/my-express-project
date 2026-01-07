34d2e309bb78267e871ed555b05a332db5ddc7c3
rajpureaishwaryaadhik-star <rajpureaishwaryaadhik@gmail.com>
2026-01-07 15:08:23 +0530

    Fix sequelize config for Render

--- config/config.js
+++ config/config.js
@@ -1,25 +1,20 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
     dialect: 'postgres',
     logging: console.log
   },
+production: {
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+  logging: false
+}
 
-  test: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    logging: false
-  },
-
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    logging: false
-  }
 };
--- database/models/index.js
+++ database/models/index.js
@@ -1,28 +1,43 @@
 'use strict';
 
-const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+  });
+}
+
+else {
+  // Local development: no SSL
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      dialect: 'postgres',
+      logging: console.log
     }
-  }
-});
+  );
+}
 
 const db = {};
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require('./user')(sequelize, Sequelize.DataTypes);
-db.Course = require('./course')(sequelize, Sequelize.DataTypes);
-db.UserCourse = require('./userCourse')(sequelize, Sequelize.DataTypes);
+// Models
+db.User = require('./user')(sequelize, DataTypes);
+db.Course = require('./course')(sequelize, DataTypes);
+db.UserCourse = require('./userCourse')(sequelize, DataTypes);
 
-// associations
-Object.keys(db).forEach((modelName) => {
+// Associations
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
